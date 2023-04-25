3806ec4e5b18d23a36da5b7a70cf42f112fe6f32
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-25 15:12:23 +0200

    Create MenuItems Collection Model

---
+++ backend/model/MenuItems.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 40
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity : {
+        type: Number,
+        required: true
+    },
+    category : {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('MenuItems', schema);
