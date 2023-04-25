39ad02b521297b8a3b061cbc563520e2dab91d4e
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-25 16:15:56 +0200

    Create and Get tables

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,16 @@ app.use(express.json())
 app.use(express.static('frontend'));
 app.use(express.urlencoded({ extended: false })) //when extended property is set to false, the URL-encoded data will instead be parsed with the query-string library.
 
+const { getAll, add }  = require('../backend/controller/tableController')
 
 // Create DB connection
 createDBConnection()
 
+// Show all albums
+app.get('/', getAll); // sends the req and res aoutomatically
+
+// Create a new album
+app.post('/', add);
 
 // LISTENING ON PORT 
 const port = process.env.PORT || 3000;
