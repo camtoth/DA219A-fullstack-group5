a3c70267fd750eb3079b2cd6615f5152f4f07e28
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-25 15:12:11 +0200

    Create Accounts Collection Model

---
+++ backend/model/Accounts.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+const schema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxlength: 25
+    },
+    username: {
+        type: String,
+        required: true,
+        maxlength: 15
+    },
+    password: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'waiter', 'cook']
+    },
+});
+
+module.exports = mongoose.model('Accounts', schema);
