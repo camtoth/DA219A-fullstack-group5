d0fd2ea2404a3ccaf03f83bd71b57776db056604
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-26 10:52:09 +0200

    Update MenuItems.js

--- backend/model/MenuItems.js
+++ backend/model/MenuItems.js
@@ -10,7 +10,7 @@ const schema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    quantity : {
+    portionSize : {
         type: Number,
         required: true
     },
