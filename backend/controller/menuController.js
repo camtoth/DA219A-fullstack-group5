7c1c4442d429b01e0d499ac21f6d828e9497d940
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-28 08:11:23 +0200

    Menu Items CRUD API

--- backend/controller/menuController.js
+++ backend/controller/menuController.js
@@ -16,4 +16,69 @@ async function getAllMenuItems(req, res) {
   }
 }
 
-module.exports = { getAllMenuItems };
+// Create a new record
+async function addMenuItem(req, res) {
+  console.log("trying to add a menu item..")
+  try {
+    // Create a new object
+    let newMenuItem = new MenuItems({
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category
+    });
+
+    // Find if it exsists is in the DB already
+    const menuItem = await MenuItems.find({
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category
+    })
+
+    // If it EXISTS in the DB
+    if (menuItem.length > 0) return res.status(409).json({ error: "The record is already in the database" })
+
+    // If not create
+    MenuItems.insertMany(newMenuItem)  // add to the db
+    res.status(201).json(menuItem) // return as a JSON object + HTTP-status code 201 (created).   
+
+  } catch (error) {
+    res.status(500).json({ error: "Server error" + error })
+  }
+}
+
+// Update a record
+async function updateMenuItem(req, res) {
+  try {
+    const menuItem = await MenuItems
+      .findByIdAndUpdate(
+        req.params.id,
+        {
+          name: req.body.name,
+          price: req.body.price,
+          category: req.body.category
+        },
+        { new: true });
+
+    // If not found - when var is null -> !var == true
+    if (!menuItem) return res.status(404).json({ error: 'ID not found' });
+
+    res.status(200).json(menuItem)
+  } catch (error) {
+    res.status(500).json({ error: "Server error" + error })
+  }
+}
+
+// Delete a record
+async function deleteMenuItem(req, res) {
+  try {
+    const menuItem = await MenuItems.findByIdAndRemove(req.params.id);
+
+    if (!menuItem) return res.status(404).json({ error: 'ID not found' });
+
+    res.status(200).json(menuItem);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" + error })
+  }
+}
+
+module.exports = { getAllMenuItems, addMenuItem, updateMenuItem, deleteMenuItem };
--- backend/routes/menuItems.js
+++ backend/routes/menuItems.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMenuItems }  = require('../controller/menuController')
+const { getAllMenuItems, addMenuItem, updateMenuItem, deleteMenuItem }  = require('../controller/menuController')
 
 
 // Show all menu items
-router.get('/Menu', getAllMenuItems); // sends the req and res aoutomatically
+router.get('/', getAllMenuItems); // sends the req and res aoutomatically
+
+// Create
+router.post('/', addMenuItem);
+
+// Update
+router.put('/:id', updateMenuItem);
+
+// Delete
+router.delete('/:id', deleteMenuItem);
 
 module.exports = router;
