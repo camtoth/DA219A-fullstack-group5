874541470c9b9abcdc3f226d957c8755508818dd
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-04-28 09:41:22 +0200

    Accounts CRUD API

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-//const { getAllTables, addTable }  = require('../controller/accountController')
+const { getAllAccounts, addAccount, updateAccount, deleteAccount }  = require('../controller/accountController')
 
 
+// Show all menu items
+router.get('/', getAllAccounts); // sends the req and res aoutomatically
+
+// Create
+router.post('/', addAccount);
+
+// Update
+router.put('/:id', updateAccount);
+
+// Delete
+router.delete('/:id', deleteAccount);
 
 module.exports = router;
-
