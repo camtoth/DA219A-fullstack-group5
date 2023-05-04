5179039e4563c634fe36b3ba07c793f73b595f17
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-05-04 18:28:02 +0200

    Set up routes

--- src/backend/routes/home.js
+++ src/backend/routes/home.js
@@ -5,11 +5,22 @@ const path = require('path');
 
 // mainpage backend team
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/about/about.html' ));
 });
 
-router.get('/placeOrder', function (req, res) {
-  res.sendFile(path.join(__dirname, '../placeOrder.html'));
+router.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../frontend/login/login.html' ));
 });
+
+router.get('/admin', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../frontend/admin/index.html'));
+});
+
+router.get('/admin/placeOrder', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../frontend/admin/placeOrder.html'));
+});
+
+
+
 
 module.exports = router;  
