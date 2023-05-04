54bab16718adf3b53f9f3c2df07d6d63dda5de31
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-05-04 20:27:33 +0200

    Log in set up

--- src/backend/routes/login.js
+++ src/backend/routes/login.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt') 
+const bcrypt = require('bcrypt')
 
-const { getAllAccounts, addAccount, updateAccount, deleteAccount, getAccount } = require('../controller/accountController')
-
-
-
-const cookieJwt = ""
-/*var current_Token = ""
+var current_Token = ""
 var current_Username = ""
 var current_Role = ""
-var current_ID = ""*/
-
+var current_ID = ""
 
 
 router.get('/', function (req, res) {
@@ -23,7 +17,7 @@ router.get('/', function (req, res) {
 // Admin
 router.get('/admin', function (req, res) {
   // Access cookie
-  const cookieJwt = req.cookies.jwt;
+  //const cookieJwt = req.cookies.jwt;
 
   res.sendFile(path.join(__dirname, '../../frontend/admin/index.html'));
 });
@@ -38,58 +32,62 @@ router.get('/restaurant', function (req, res) {
 });
 
 // POST
-router.post('/', async (req, res) {
+router.post('/', async (req, res) => {
   try {
     // Get the first user with the same username
-    var user = await database.getUserByName(req.body.username)
+    var user = await (await fetch(`http://localhost:3000/api/accounts/${req.body.username}`)).json();
 
+    console.log(user);
     // Dose user exist and is password correct
     if (user != null && await bcrypt.compare(req.body.password, user.password)) {
 
-       // Create a signed token
-       const payload = {
-          username: user.username,
-          password: user.password
-       }
+      console.log("The password is correct");
 
-       // Create and log the token
-       var token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET)
-     
-       // Save the user session info
-       current_Token = token
-       current_Username = user.username
-       current_Role = user.role
-       current_ID = user.userID
+      // Create a token object
+      const payload = {
+        username: user.username,
+        password: user.password
+      }
 
-       // Successful log in
-       if (current_Role == "admin") {
-          res.redirect("/admin")
-       } else {
-          res.redirect("/users/" + user.userID)
-          // res.redirect("/granted")
-       }
-       
+      // Create JWT token
+      const token = jwt.sign(payload, process.env.SECRET_PRIVATE_KEY, { expiresIn: '1h' });
+
+      // Set JWT token as cookie
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict'
+      });
+
+      // Save the user session info
+      current_Token = token
+      // We can access the cookie from everywhere with:
+      // const cookieJwt = req.cookies.jwt;
+
+      current_Username = user.username
+      current_Role = user.role
+      current_ID = user._id
+
+      // Successful log in
+      if (current_Role == "admin") {
+        //res.redirect("/admin")
+        console.log("logged admin");
+      } else if (current_Role == "waiter"){
+        console.log("Logged waiter");
+        //res.redirect("/users/" + user.userID)
+        // res.redirect("/granted")
+      }
+
     } else {
-       // Failed log in
-       res.render('fail.ejs')
+      // Failed log in
+      //res.render('fail.ejs')
+      console.log("Failed log in");
     }
 
- } catch (error) {
+  } catch (error) {
     console.log(error)
- }
+  }
 
-  console.log(req.body);
-  // Create JWT token
-  const token = jwt.sign({ data: 'foo' }, process.env.SECRET_PRIVATE_KEY, { expiresIn: '1h' });
-
-  // Set JWT token as cookie
-  res.cookie('jwt', token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'strict'
-  });
-
-  
 });
 
 
