9166aa9577a002ca75205bc30938f7e77f72336a
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2023-05-04 20:10:58 +0200

    Get account by Username

--- src/backend/controller/accountController.js
+++ src/backend/controller/accountController.js
@@ -13,6 +13,23 @@ async function getAccount(req, res) {
     res.status(result[0]).json(result[1]);
 }
 
+//get account by name
+async function getAccountByUsername(req, res) {
+    try {
+        const user = await Accounts.findOne({ username: req.params.username })
+
+        // If id was not found
+        if (user.length == 0) return res.status(404).json({ error: "Username not found" })
+
+        res.status(200).json(user)
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Server error" + error })
+    }
+}
+
+
 //add a new account
 async function addAccount(req, res) {
     const result = await addRecord("accounts", req);
@@ -32,4 +49,4 @@ async function updateAccount(req, res) {
 }
 
 
-module.exports = { getAllAccounts, addAccount, updateAccount, deleteAccount, getAccount };
+module.exports = { getAllAccounts, addAccount, updateAccount, deleteAccount, getAccount, getAccountByUsername};
--- src/backend/routes/accounts.js
+++ src/backend/routes/accounts.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllAccounts, addAccount, updateAccount, deleteAccount, getAccount } = require('../controller/accountController')
+const { getAllAccounts, addAccount, updateAccount, deleteAccount, getAccount, getAccountByUsername } = require('../controller/accountController')
 
 
 // Show all
 router.get('/', getAllAccounts); // sends the req and res aoutomatically
+
+// Get account by username
+router.get('/:username', getAccountByUsername); // sends the req and res aoutomatically
+
 
 // Create
 router.post('/', addAccount);
