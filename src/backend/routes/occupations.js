7013c56701d87e639da8af9e511c3833092a657d
Stephan0027 <stephan.hek0027@stud.hkr.se>
2023-05-04 13:44:43 +0200

    add order with push command

--- src/backend/routes/occupations.js
+++ src/backend/routes/occupations.js
@@ -9,7 +9,7 @@ router.get('/', getAllOccupations); // sends the req and res aoutomatically
 router.get('/current', getCurrentOccupations); // Return array of all occuptions at the current date and time.
 router.get('/orders/:id', getOccupationOrder) //get orders from occupation
 router.get('/:id', getOccupation); // Return occuptions by ID
-//router.put('/placeOrder/:id', addOccupationOrder) // Occupation order
+router.put('/placeOrder/:id', addOccupationOrder) // add order to occupation
 
 
 // Create new occupation
